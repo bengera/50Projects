4f950533aade48b3a8ecce7d1aec2ca95dba06e5
unknown <bengera89@gmail.com>
2022-01-10 23:52:37 +0700

    to-do-list

--- practice/script.js
+++ practice/script.js
@@ -1,20 +1,69 @@
 const container = document.querySelector('.container')
 const button = document.getElementById('button');
+const inputField = document.getElementById('inputField')
+const taskContainer = document.querySelector('.tasks-container')
+
 
 button.addEventListener('click', () => {
+    
     console.log('button-clicked')
 
-    //CREATE DIV
-    const div = document.createElement("div")
-    div.setAttribute("id", "div-task")
-    const divContent = document.createTextNode("This is a task")
-    div.appendChild(divContent);
-    container.appendChild(div)
-    
+    const taskItem = document.createElement('div')
+    taskItem.setAttribute("id", "task")
+    const taskItemText = document.createElement('p')
+    taskItemText.setAttribute("id", "task-text")
+    taskItemText.innerText = inputField.value;
+    taskContainer.appendChild(taskItem);
+    taskItem.appendChild(taskItemText);
+    inputField.value = '';
 
-    //CREATE BUTTON
     const newbtn = document.createElement('BUTTON')
-    newbtn.setAttribute("id", "new-button")
-    newbtn.innerHTML = "Created button"
-    container.appendChild(newbtn);
+        newbtn.setAttribute("id", "new-button")
+        newbtn.innerHTML = "Delete"
+        taskItem.appendChild(newbtn);
+
+        newbtn.addEventListener('click', () => {
+            console.log('deleted-item')
+            taskItem.remove();
+        })
+
 })
+
+// button.addEventListener('click', () => {
+//     console.log('button-clicked')
+
+//     //CREATE DIV
+//     const div = document.createElement("div")
+//     div.setAttribute("id", "div-task")
+//     const divContent = document.createTextNode("This is a task")
+//     div.appendChild(divContent);
+//     container.appendChild(div)
+    
+//     //CREATE BUTTON
+//     const newbtn = document.createElement('BUTTON')
+//     newbtn.setAttribute("id", "new-button")
+//     newbtn.innerHTML = "Delete"
+//     container.appendChild(newbtn);
+// })
+
+
+
+/*
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
+*/
